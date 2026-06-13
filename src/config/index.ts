2357fd02fe7d920559016f268d2208f45da56c9a
applicationmate <applicationmate.tech@gmail.com>
2026-06-13 18:47:36 -0400

    Initial db setup wit config file

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
+
+const config = {
+  port: process.env.PORT,
+  connection_string: process.env.connection_string,
+};
+
+export default config;
---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import { Pool } from "pg";
+import config from "../config";
+
+export const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+export const initDB = async () => {
+  try {
+  } catch (error) {}
+};
