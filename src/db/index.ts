d4a0e7caf948ae5619fe1615bc4455e0ed9a33ef
applicationmate <applicationmate.tech@gmail.com>
2026-06-15 22:40:11 -0400

    (db): issues table added

--- src/db/index.ts
+++ src/db/index.ts
@@ -16,13 +16,26 @@ export const initDB = async () => {
         name TEXT,
         email VARCHAR(100) UNIQUE NOT NULL,
         password TEXT NOT NULL,
-        role varchar(15) DEFAULT 'contributor',
+        role varchar(15) NOT NULL DEFAULT 'contributor',
 
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
         )
         `);
+    await pool.query(`
+                CREATE TABLE IF NOT EXISTS issues(
+                id SERIAL PRIMARY KEY,
+                reporter_id INT NOT NULL,
 
+                title VARCHAR(150) NOT NULL ,
+                description TEXT NOT NULL,
+                type VARCHAR(20) NOT NULL,
+                status VARCHAR(25) NOT NULL,
+
+                 created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+                )
+                `);
     console.log("Database Connected!");
   } catch (error) {
     console.log(error);
