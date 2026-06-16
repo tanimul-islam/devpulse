1ace00c6beff49d2c6376303206a90054c888fa8
applicationmate <applicationmate.tech@gmail.com>
2026-06-16 15:36:41 -0400

    (cors): options added

--- src/App.ts
+++ src/App.ts
@@ -7,11 +7,17 @@ import express, {
 import { authRoute } from "./modules/auth/auth.route";
 import { globalErrorHandler } from "./middleware/globalErrorHandler";
 import { issueRoute } from "./modules/issues/issues.route";
+import cors from "cors";
 
 const app: Application = express();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
 
 app.use(express.json());
 express.urlencoded({ extended: true });
+app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoute);
 app.use("/api/issues", issueRoute);
